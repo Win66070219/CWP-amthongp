601a5d7af4d46d325baccda88103d07bd1553744
Win66070219 <66070219@kmitl.ac.th>
2026-09-21 16:21:58 +0700

    feat: complete cell 02 exercise 01

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+inp = int(input(""))
+
+if inp > 0:
+    print("This number is positive.")
+elif inp < 0:
+    print("This number is negative.")
+elif inp == 0:
+    print("This number is both positive and negative.")
